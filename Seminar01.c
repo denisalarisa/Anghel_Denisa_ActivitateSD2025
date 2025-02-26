f397353c5dc597bfe581802e7f8b82d7564dc45f
Denisa <Denisa@DESKTOP-65A2J7R>
2025-02-26 21:47:54 +0200

    Add project files.

---
+++ Seminar01.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct  Masina {
+	int id;
+	int nrLocuri;
+	char* marca;
+	float capacitateC;
+	char normaPoluare;
+};
+
+struct Masina initializare(int id, int nrLocuri, const char* marca, float capacitateC, char  normaPoluare) {
+	struct Masina m;
+	m.id = id;
+	m.nrLocuri = nrLocuri;
+	m.capacitateC = capacitateC;
+	m.normaPoluare = normaPoluare;
+	m.marca = (char*)malloc(strlen(marca) + 1);
+	strcpy_s(m.marca, strlen(marca) + 1, marca);
+	return m;
+}
+
+void afisare(struct Masina m) {
+	printf("id masina: %d\n Nr. Locuri: %d\n capacitateC : %f\n Marca: %s\n Euro%c\n", m.id, m.nrLocuri, m.capacitateC, m.marca, m.normaPoluare);
+}
+
+void modifica_Atribut(struct Masina* m, int nrNou) {
+	if (nrNou > 0) {
+		m->nrLocuri = nrNou;
+	}
+}
+
+void dezalocare(struct Masina* m) {
+	free(m->marca);
+	(*m).marca = NULL;
+}
+
+int main() {
+	struct Masina masina =  initializare(1, 5, "Dacia", 1.5, '4'); //"4" '4' 
+	afisare(masina);
+
+	modifica_Atribut(&masina, 6);
+	afisare(masina);
+
+	dezalocare(&masina);
+	afisare(masina);
+
+	printf("%d\n", sizeof(struct Masina*));
+	printf("%d", sizeof(masina));
+
+
+}
